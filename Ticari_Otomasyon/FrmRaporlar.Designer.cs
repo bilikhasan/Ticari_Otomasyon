8872c3da7df47844ea25d591887e59246d8bfe90
bilikhasan <fatherlyy@hotmail.com.tr>
2024-09-29 13:50:45 +0300

    Bölüm 16: Raporlar Modülü bitti

---
+++ Ticari_Otomasyon/FrmRaporlar.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Ticari_Otomasyon
+{
+    partial class FrmRaporlar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmRaporlar
+            // 
+            this.ClientSize = new System.Drawing.Size(1203, 420);
+            this.Name = "FrmRaporlar";
+            this.Text = "RAPORLAR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraTab.XtraTabControl xtraTabControl1;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage1;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage2;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage3;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage4;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage5;
+
+        private DevExpress.XtraBars.Docking.DockPanel panelContainer3;
+        private DevExpress.XtraBars.Docking.DockPanel panelContainer2;
+        private DevExpress.XtraBars.Docking.DockPanel dockPanel1;
+        private DevExpress.XtraReports.UserDesigner.DesignControlContainer designControlContainer1;
+        private DevExpress.XtraReports.UserDesigner.ErrorListDockPanel errorListDockPanel1;
+        private DevExpress.XtraReports.UserDesigner.GroupAndSortDockPanel groupAndSortDockPanel1;
+        private DevExpress.XtraReports.UserDesigner.ReportGalleryDockPanel reportGalleryDockPanel1;
+        private DevExpress.XtraReports.UserDesigner.ReportExplorerDockPanel reportExplorerDockPanel1;
+        private DevExpress.XtraReports.UserDesigner.PropertyGridDockPanel propertyGridDockPanel1;
+        private DevExpress.XtraReports.UserDesigner.FieldListDockPanel fieldListDockPanel1;
+        private DevExpress.XtraBars.Bar bar2;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiZoomIn;
+        private DevExpress.XtraReports.UserDesigner.DesignRepositoryItemComboBox designRepositoryItemComboBox2;
+        private DevExpress.XtraReports.UserDesigner.XRZoomBarEditItem bbiZoom;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiZoomOut;
+        private DevExpress.XtraBars.Bar bar1;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem11;
+        private DevExpress.XtraBars.BarMdiChildrenListItem msiWindows;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem10;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem9;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem8;
+        private DevExpress.XtraBars.BarSubItem msiWindow;
+        private DevExpress.XtraReports.UserDesigner.CommandBarCheckItem msiWindowInterface;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem7;
+        private DevExpress.XtraBars.BarSubItem msiOrder;
+        private DevExpress.XtraBars.BarSubItem bsiCenter;
+        private DevExpress.XtraBars.BarSubItem msiVerticalSpacing;
+        private DevExpress.XtraBars.BarSubItem msiHorizontalSpacing;
+        private DevExpress.XtraBars.BarSubItem msiSameSize;
+        private DevExpress.XtraBars.BarSubItem msiAlign;
+        private DevExpress.XtraBars.BarSubItem msiJustify;
+        private DevExpress.XtraBars.BarSubItem msiFont;
+        private DevExpress.XtraBars.BarSubItem msiEdit;
+        private DevExpress.XtraBars.BarSubItem msiFile;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem6;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem5;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem4;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem3;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem2;
+        private DevExpress.XtraBars.BarSubItem msiFormat;
+        private DevExpress.XtraBars.BarSubItem barSubItem2;
+        private DevExpress.XtraReports.UserDesigner.BarDockPanelsListItem barDockPanelsListItem1;
+        private DevExpress.XtraBars.BarSubItem barSubItem1;
+        private DevExpress.XtraReports.UserDesigner.XRBarToolbarsListItem xrBarToolbarsListItem1;
+        private DevExpress.XtraBars.BarSubItem msiTabButtons;
+        private DevExpress.XtraReports.UserDesigner.BarReportTabButtonsListItem barReportTabButtonsListItem1;
+        private DevExpress.XtraBars.BarStaticItem bsiHint;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiRedo;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiUndo;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiPaste;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiCopy;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiCut;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiSaveFile;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiOpenFile;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem commandBarItem1;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiSendToBack;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiBringToFront;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiCenterVertically;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiCenterHorizontally;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiVertSpaceConcatenate;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiVertSpaceDecrease;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiVertSpaceIncrease;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiVertSpaceMakeEqual;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiHorizSpaceConcatenate;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiHorizSpaceDecrease;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiHorizSpaceIncrease;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiHorizSpaceMakeEqual;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiSizeToControl;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiSizeToControlHeight;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiSizeToGrid;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiSizeToControlWidth;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiAlignBottom;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiAlignHorizontalCenters;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiAlignTop;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiAlignRight;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiAlignVerticalCenters;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiAlignLeft;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiAlignToGrid;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiJustifyJustify;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiJustifyRight;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiJustifyCenter;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiJustifyLeft;
+        private DevExpress.XtraReports.UserDesigner.CommandColorBarItem bbiBackColor;
+        private DevExpress.XtraReports.UserDesigner.CommandColorBarItem bbiForeColor;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiFontUnderline;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiFontItalic;
+        private DevExpress.XtraReports.UserDesigner.CommandBarItem bbiFontBold;
+        private DevExpress.XtraBars.BarEditItem beiFontSize;
+        private DevExpress.XtraBars.BarEditItem beiFontName;
+        private DevExpress.XtraReports.UserDesigner.DesignRepositoryItemComboBox designRepositoryItemComboBox1;
+        private DevExpress.XtraReports.UserDesigner.RecentlyUsedItemsComboBox recentlyUsedItemsComboBox1;
+        private DevExpress.XtraReports.UserDesigner.DesignBar designBar5;
+        private DevExpress.XtraReports.UserDesigner.DesignBar designBar4;
+        private DevExpress.XtraReports.UserDesigner.DesignBar designBar3;
+        private DevExpress.XtraReports.UserDesigner.DesignBar designBar2;
+        private DevExpress.XtraReports.UserDesigner.DesignBar designBar1;
+    }
+}
